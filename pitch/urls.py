646608ec15bf6cbad643cdde15da7c4c22aa6e2b
niraj-khatiwada <nirazzz.nk@gmail.com>
2020-05-25 23:13:24 +0545

    Deploying

--- pitch/urls.py
+++ pitch/urls.py
@@ -6,5 +6,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('bank.rest_api.urls')),
-    re_path('.*', TemplateView.as_view('index.html'))
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
 ]
